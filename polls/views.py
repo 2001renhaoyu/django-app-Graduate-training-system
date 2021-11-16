513429980f194cc24e28bb6171c0422d61b537ac
2001renhaoyu <2001renhaoyu@gmail.com>
2021-11-16 15:28:28 +0800

    rhy web

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "student/student_index.html", {})
+    return render(request, "teacher/teacher_index.html", {})
