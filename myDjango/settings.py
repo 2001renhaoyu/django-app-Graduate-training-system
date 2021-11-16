cd99bdeb113e836f823ed13606ec4df6792e3942
ColdSama520 <1360614758@qq.com>
2021-11-17 00:22:12 +0800

    cold

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'myDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'NAME': 'CourseDesign',
-        'ENGINE': 'sql_server.pyodbc',
-        'HOST': '127.0.0.1',
-        'PORT': '1433',
-        'USER': 'admin921',
-        'PASSWORD': '921',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'NAME': 'CourseDesign',
+#         'ENGINE': 'sql_server.pyodbc',
+#         'HOST': '127.0.0.1',
+#         'PORT': '1433',
+#         'USER': 'admin921',
+#         'PASSWORD': '921',
+#         'OPTIONS': {
+#             'driver': 'ODBC Driver 17 for SQL Server',
+#         }
+#     }
+# }
 
 
 # Password validation
