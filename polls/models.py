ccfb944e99b0024fcf9b2569f12520e5c1be491c
shenyunkai <252442498@qq.com>
2021-11-16 19:18:56 +0800

    学术交流模型建立 --syk

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AcademicActivity(models.Model):
+    aca_activity_id = models.CharField(max_length=30)
+    aca_activity_name = models.CharField(max_length=30)
+    aca_student_id = models.CharField(max_length=30)
+    aca_activity_location = models.CharField(max_length=30)
+    aca_activity_date = models.CharField(max_length=20)
+    aca_report_name_zh = models.CharField(max_length=30)
+    aca_report_name_en = models.CharField(max_length=50)
+    aca_evidentiary_material = models.CharField(max_length=50)
+    aca_audit_situation = models.CharField(max_length=20)
+    aca_extra = models.CharField(max_length=100)
