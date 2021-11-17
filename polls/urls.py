4374f6f9b86a95e63e01a0a709db8629e4cd3cbd
shenyunkai <252442498@qq.com>
2021-11-17 14:24:36 +0800

    syk

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     # path('', views.login),
     path('', views.teacher_test),
-    path('teacher_assistant_volunteer_apply', views.teacher_assistant_volunteer_apply),
+    path('teacher_assistant_volunteer_apply',
+         views.teacher_assistant_volunteer_apply),
+
+    # student
+    path('student', views.student_index),
+    path('student/show_academic_activity', views.show_student_avtivity),
+    path('student/student_activity_form', views.student_activity_form),
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from polls.models import *
+from polls import academic_activity
 # Create your views here.
 
 
@@ -12,3 +14,16 @@ def teacher_test(request):
 
 def teacher_assistant_volunteer_apply(request):
     return render(request, "teacher/teacher_assistant_volunteer_apply.html", {})
+
+
+def student_index(request):
+    return render(request, 'student/student_index.html', {})
+
+
+def show_student_avtivity(request):
+    lists = academic_activity.get_academic_activity_list('s002')
+    return render(request, 'student/student_academic_activity.html', {'activity_list': lists})
+
+
+def student_activity_form(request):
+    return render(request, 'student/student_academic_activity.html', {'activity_list': None})
