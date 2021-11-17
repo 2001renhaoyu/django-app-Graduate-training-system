4f9c4947bf5cf3db5f7b802172a3c425603fa64a
ColdSama520 <1360614758@qq.com>
2021-11-17 15:18:32 +0800

    cold

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ def student_index(request):
     return render(request, 'student/student_index.html', {})
 
 
-def show_student_avtivity(request):
+def show_student_activity(request):
     lists = academic_activity.get_academic_activity_list('s002')
     return render(request, 'student/student_academic_activity.html', {'activity_list': lists})
 
